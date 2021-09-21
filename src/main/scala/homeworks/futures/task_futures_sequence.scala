e8411e3840e61940bf6f89e4556363ddb91afcdd
ADzhura <dzhura31@yandex.ru>
2021-09-21 22:33:21 +0300

    convert list of futures into future of results and exceptions tuple

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -2,7 +2,9 @@ package homeworks.futures
 
 import homeworks.HomeworksUtils.TaskSyntax
 
+import scala.concurrent.Future.successful
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object task_futures_sequence {
 
@@ -15,11 +17,18 @@ object task_futures_sequence {
    * в правово результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    val initFuture = Future.successful((List.empty[A], List.empty[Throwable]))
+    futures.foldLeft(initFuture)((flist, future) =>
+      flist.flatMap { case (alist, elist) =>
+        future.map { v => (alist :+ v, elist) }.recover { error => (alist, elist :+ error) }
+      }
+    )
+  }
 }
